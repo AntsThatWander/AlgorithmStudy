d2797aa0bc1b38cfb02b1485891f56fe03271bea
AntsThatWander <anubspider@gmail.com>
2025-04-18 15:14:57 +0900

    [Gold V] Title: Z, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/1074. Z/README.md
+++ 백준/Gold/1074. Z/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 14252 KB, 시간: 112 ms
+메모리: 14212 KB, 시간: 108 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2025년 4월 17일 21:34:57
+2025년 4월 18일 15:14:38
 
 ### 문제 설명
 
@@ -24,13 +24,13 @@
 
 <p>다음 예는 2<sup>2</sup> × 2<sup>2</sup> 크기의 배열을 방문한 순서이다.</p>
 
-<p style="text-align:center"><img alt="" src="https://u.acmicpc.net/adc7cfae-e84d-4d5c-af8e-ee011f8fff8f/Screen%20Shot%202020-12-02%20at%208.11.17%20AM.png" style="width: 250px; height: 252px;"></p>
+<p style="text-align:center"><img alt="" src="" style="width: 250px; height: 252px;"></p>
 
 <p>N이 주어졌을 때, r행 c열을 몇 번째로 방문하는지 출력하는 프로그램을 작성하시오.</p>
 
 <p>다음은 N=3일 때의 예이다.</p>
 
-<p style="text-align:center"><img alt="" src="https://u.acmicpc.net/d3e84bb7-9424-4764-ad3a-811e7fcbd53f/Screen%20Shot%202020-12-30%20at%2010.50.47%20PM.png" style="width: 533px; height: 535px;"></p>
+<p style="text-align:center"><img alt="" src="" style="width: 533px; height: 535px;"></p>
 
 ### 입력 
 
--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,29 +15,40 @@ public class Main {
 
             int sequence = 0;
 
-            Stack<ZArray> myCallStack = new Stack<>();
-            myCallStack.add(new ZArray(n, row, col));
-
-            while(!myCallStack.isEmpty()) {
-                ZArray zArray = myCallStack.pop();
-
-                if (zArray.getN() == 1) {
-                    sequence += zArray.getRow() * 2 + zArray.getCol();
+            // N=n일 때, 2^(n-1) * 2^(n-1) 박스 4개로 나눈 후, 1번째 박스에 있다고 가정하면서 문제를 푼다.
+            // 예를 들어, N=2일 때, (3,1) -> (1,1)로 이동한다.
+            // 이때, 반드시 이동한만큼 순서 값을 보정해줘야 하는데, 이 공식이 nMoved이다.
+            
+            // 이동 및 보정할 때 row와 col을 따로 계산하는데, 좌표값 이동은 %로 하며, 한 line별이므로 2^(n-1)로만 계산하는 것이다.
+            // 또 보정은 line별로 /를 하고, 2^(n-1) * 2^(n-1)로 추가로 보정한다.
+            
+            // 설명이 개판이니 예시를 하나 써놓겠다.
+            // N=2 일 때, (3,1)이라고 하자
+            // (3,1) -> (1,1)로 이동시킨다고 하자.
+            // 이떄, 위치 이동은 2^(n-1)을 나눈 나머지로 한다. 즉, 2^(2-1)로 나눈 나머지로 (3%2, 1%1)로 이동.
+            // 이동 값 보정은 3/2, 1/2 -> 1, 0을 한 후, 2^(2-1)*2^(2-1)=4를 이용해 보정한다. 식은 아래와 같다.
+            // 2(Z자 이동이어서 row값이 비중이 더 크고, 그 값에 대한 비율) * 4(위 2^(2-1)*2^(2-1)) * 1(3/2로 얻은 값) + 4(위 2^(2-1)*2^(2-1)) * 0(1/1로 얻은 값)
+            // 왜 몫을 구하는고 하니 박스를 크게 4등분했을 때 (0n,0n) (0n,1n) (1n,0n) (1n,1n) 에서 n을 제거하려고 하는 것.
+            // 나머지는 위에도 설명했다시피 4등분했을 때 등분된 사각형에서 어디 위치에 있는지 파악하는 용도
+            for (int i=n; i>=1; --i) {
+                if (i == 1) {
+                    sequence += row * 2 + col;
                     continue;
                 }
 
-                int tmpN = zArray.getN() - 1;
+                int tmpN = i - 1;
                 int prevPowLine = (int)Math.pow(2, tmpN);
                 int prevPowBox = (int)Math.pow(prevPowLine, 2);
 
-                int rowQuotient = zArray.getRow() / prevPowLine;
-                int colQuotient = zArray.getCol() / prevPowLine;
+                int rowQuotient = row / prevPowLine;
+                int colQuotient = col / prevPowLine;
                 int nMoved = prevPowBox * rowQuotient * 2 + prevPowBox * colQuotient;
                 sequence += nMoved;
 
-                int rowRemain = zArray.getRow() % prevPowLine;
-                int colRemain = zArray.getCol() % prevPowLine;
-                myCallStack.add(new ZArray(tmpN, rowRemain, colRemain));
+                int rowRemain = row % prevPowLine;
+                int colRemain = col % prevPowLine;
+                row = rowRemain;
+                col = colRemain;
             }
 
             System.out.println(sequence);
@@ -48,28 +58,4 @@ public class Main {
         }
     }
 
-}
-
-class ZArray {
-    private int n;
-    private int row;
-    private int col;
-
-    public ZArray(int n, int row, int col) {
-        this.n = n;
-        this.row = row;
-        this.col = col;
-    }
-
-    public int getN() {
-        return n;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
-    }
 }
