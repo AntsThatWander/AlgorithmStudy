92a5a6829b7142339c1c1b859d1b271caa24e080
AntsThatWander <anubspider@gmail.com>
2025-04-19 22:00:52 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -3,30 +3,28 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
-            int testcase = Integer.parseInt(br.readLine());
-            int[] scores = new int[testcase];
 
-            for (int i=0; i<testcase; ++i) {
-                scores[i] = Integer.parseInt(br.readLine());
-            }
+        int testcase = Integer.parseInt(br.readLine());
+        int[] scores = new int[testcase];
 
-            int reduce = 0;
-            for (int i=testcase-2; i>=0; --i) {
-                if (scores[i] >= scores[i+1]) {
-                    int toReduce = Math.abs(scores[i+1] - scores[i]) + 1;
-                    scores[i] -= toReduce;
-                    reduce += toReduce;
-                }
-                
+        for (int i=0; i<testcase; ++i) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
+
+        int reduce = 0;
+        for (int i=testcase-2; i>=0; --i) {
+            if (scores[i] >= scores[i+1]) {
+                int toReduce = Math.abs(scores[i+1] - scores[i]) + 1;
+                scores[i] -= toReduce;
+                reduce += toReduce;
             }
-            System.out.println(reduce);
-            br.close();
-        } catch (IOException ioe) {
 
         }
+        System.out.println(reduce);
+        br.close();
+
     }
 }
