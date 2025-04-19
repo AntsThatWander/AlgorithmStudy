bf598949005677adda5d07ad035bbbcc08a3e090
AntsThatWander <anubspider@gmail.com>
2025-04-19 21:52:37 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int testcase = Integer.parseInt(br.readLine());
+            int[] scores = new int[testcase];
+
+            for (int i=0; i<testcase; ++i) {
+                scores[i] = Integer.parseInt(br.readLine());
+            }
+
+            int reduce = 0;
+            for (int i=testcase-2; i>=0; --i) {
+                if (scores[i] >= scores[i+1]) {
+                    int toReduce = Math.abs(scores[i+1] - scores[i]) + 1;
+                    scores[i] -= toReduce;
+                    reduce += toReduce;
+                }
+                
+            }
+            System.out.println(reduce);
+            br.close();
+        } catch (IOException ioe) {
+
+        }
+    }
+}
